44257690f62c7349e7e1dcdcefc62553ec0200d3
Laboratória <ma-ah02@live.com>
2019-03-26 17:11:45 -0300

    implementando cadastro

---
+++ src/config.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyCtQsKuJmvf1I5BSKJ_JHt-jRbwRIkzaBE",
+  authDomain: "rede-social-52d48.firebaseapp.com",
+  databaseURL: "https://rede-social-52d48.firebaseio.com",
+  projectId: "rede-social-52d48",
+  storageBucket: "rede-social-52d48.appspot.com",
+  messagingSenderId: "822040281221"
+};
+firebase.initializeApp(config);
+
